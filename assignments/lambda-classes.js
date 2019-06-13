429cfc45a199b80f09398e00ce5f0c7ceb042084
Neha Patel <nehatr0n@Nehas-MacBook-Pro.local>
2019-06-13 13:44:51 -0700

    class Instructor complete, starting class Student

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -9,3 +9,28 @@ class Person {
         console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
     }
 }
+
+class Instructor extends Person {
+    constructor(attrs) {
+        super(attrs);
+        this.specialty = attrs.specialty;
+        this.favLanguage = attrs.favLanguage;
+        this.catchPhrase = attrs.catchPhrase;
+        this.subject = attrs.subject;
+    }
+    demo() {
+        console.log(`Today we are learning about ${this.subject}`);
+    }
+    grade() {
+        console.log(`${Student.name} receives a perfect score on ${this.subject}`)
+    }
+}
+
+class Student extends Person {
+    constructor(attrs) {
+        super(attrs);
+        
+
+    }
+
+}
